0af464d84b18bae2135ed5ec23813fe6ae610432
John Zhang <jnzhang622@gmail.com>
2020-02-25 21:13:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(emoticons)
+  
+  emotes = {"get_meaning" => {}, "get_emoticon" => {}}
+  
+  YAML.load_file(emoticons).each do |meaning, describe|
+     eng, jan = describe
+     emotes["get_meaning"][jan] = meaning
+     emotes["get_emoticon"][eng] = jan
+  end
+  emotes
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
+  
+  
 end
 
 def get_english_meaning
-  # code goes here
+  
+  
+  
 end

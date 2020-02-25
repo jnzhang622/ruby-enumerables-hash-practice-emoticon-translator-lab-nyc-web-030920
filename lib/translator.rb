7c3b5b4654245aaf35e68ba4f6d552dbac4e2be3
John Zhang <jnzhang622@gmail.com>
2020-02-25 22:41:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,13 @@ def get_japanese_emoticon(path, emoticon)
 end
 
 def get_english_meaning
-  
+  emoticons = load_library(path)
+  result = emoticons["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
   
   
 end

2e3ab72d10c464460ac8abc0969aba4d46321210
John Zhang <jnzhang622@gmail.com>
2020-02-25 21:49:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,9 @@
 require 'yaml'
 
-def load_library(path)
+def load_library(info)
   emotes = {"get_meaning" => {}, "get_emoticon" => {}}
   
-  YAML.load_file(path).each do |meaning, emoticons|
+  YAML.load_file(info).each do |meaning, emoticons|
     eng, jap = emoticons
     emotes["get_meaning"][jap] = meaning
     emotes["get_emoticon"][eng] = jap
